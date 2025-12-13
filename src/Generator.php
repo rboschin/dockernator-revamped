a5c6ab4d874dcce31c10b6ded6c69b9bb68abbbc
rboschin <rboschin@gmail.com>
2025-12-13 17:54:52 +0100

    Adding some rumor to name

--- src/Generator.php
+++ src/Generator.php
@@ -16,14 +16,16 @@ class Generator
      *
      * @return string
      */
-    public function generate(): string
+    public function generate(String $rumor = ''): string
     {
+        $rumorString = self::setRumor($rumor);
+
         $prefix = $this->prefix[mt_rand(0, count($this->prefix) - 1)];
         $name = $this->names[mt_rand(0, count($this->names) - 1)];
 
         $sections = [$prefix, $name];
 
-        return implode($this->delimiter, array_filter($sections));
+        return implode($this->delimiter, array_filter($sections)) . $rumorString;
     }
 
     /**
@@ -36,5 +38,29 @@ class Generator
         $this->delimiter = $delimiter;
 
         return $this;
+    }
+
+    /**
+     * Adds a final string/number.
+     * 
+     * $level can be:
+     * day / date / unique / random / a custom string/number
+     *
+     * @return \Rboschin\DockernatorRevamped\Generator
+     */    public function setRumor($level)
+    {
+        switch ($level) {
+            case 'day':
+                return $this->delimiter . strtolower(date('l'));
+            case 'date':
+                return  $this->delimiter . 
+                    date("Y" . $this->delimiter . "m" . $this->delimiter . "d"); 
+            case 'unique':
+                return $this->delimiter . uniqid();
+            case 'random':
+                return $this->delimiter . mt_rand(1000, 9999);
+            default:
+                return empty($level) ? '' : $this->delimiter . $level;
+        }
     }
 }
